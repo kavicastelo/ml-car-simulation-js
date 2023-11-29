48b6f4edb876334c1184cdb048e6dbf03e67b616
kavi castelo <kavindu123450@gmail.com>
2023-11-29 19:22:42 +0530

    road

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
 
-const road = new Road(canvas.width/2,canvas.width);
-const car = new Car(100,400,30,50);
+const road = new Road(canvas.width/2,canvas.width*0.9);
+const car = new Car(road.getLaneCenter(0),400,30,50);
 
 addEventListener('resize',()=>{
     canvas.width = 200;
@@ -16,8 +16,12 @@ function animate() {
     requestAnimationFrame(animate);
     ctx.clearRect(0,0,canvas.width,canvas.height);
     car.update();
+
+    ctx.save();
+    ctx.translate(0,-car.y+canvas.height*0.7);
     road.draw(ctx);
     car.draw(ctx);
+    ctx.restore();
 }
 
 animate()
---
+++ utils.js
@@ -0,0 +1,3 @@
+function lerp(A, B, t) {  // lerp = linear interpolation
+    return A + (B - A) * t;
+}
